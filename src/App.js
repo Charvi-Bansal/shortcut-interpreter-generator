9bc6b20a10030981a6639c9d8f30602110b1c56d
Charvi Bansal <charvibansal02@gmail.com>
2022-01-30 19:54:34 +0530

    remove horizontal scroll

--- src/App.js
+++ src/App.js
@@ -1,55 +1,54 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./styles.css";
 // import Button from 'react-bootstrap/Button'
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 
 var shortcuts = {
   /* general windows shortcuts */
-  "Copy" : "ctrl + C",
-  "Paste" : "ctrl + V",
-  "Save" : "ctrl + S",
-  "Undo" : "ctrl + Z",
-  "Redo" : "ctrl + Y",
-  "Run" : "windows + R",
-  "Find window/app" : "ctrl + F",
-  "Minimise all windows" : "windows + D",
-  "Window tab move" : "alt + tab",
-  "Application tab move" : "ctrl + tab",
-  "Reverse application tab move" : "shift + ctrl + tab",
+  Copy: "ctrl + C",
+  Paste: "ctrl + V",
+  Save: "ctrl + S",
+  Undo: "ctrl + Z",
+  Redo: "ctrl + Y",
+  Run: "windows + R",
+  "Find window/app": "ctrl + F",
+  "Minimise all windows": "windows + D",
+  "Window tab move": "alt + tab",
+  "Application tab move": "ctrl + tab",
+  "Reverse application tab move": "shift + ctrl + tab",
 
   /* VS code shortcuts */
-  "Go back" : "alt + left arrow",
-  "Go forward" : "alt + right arrow",
-  "Find in all files" : "ctrl + shift + F",
-  "Comment" : "ctrl + K + C",
-  "Uncomment" : "ctrl + K + U",
-  "Comment Toggle" : "ctrl + /",
-  "Duplicate line above" : "alt + shift + up arrow",
-  "Duplicate line below" : "alt + shift + down arrow",
-  "Refractor" : "ctrl + shift + R",
-  "Rename" : "F2",
-  "Goto definition" : "F12",
-  "Multi-cursor" : "ctrl + D",
-  "Quick find files" : "ctrl + P"
+  "Go back": "alt + left arrow",
+  "Go forward": "alt + right arrow",
+  "Find in all files": "ctrl + shift + F",
+  Comment: "ctrl + K + C",
+  Uncomment: "ctrl + K + U",
+  "Comment Toggle": "ctrl + /",
+  "Duplicate line above": "alt + shift + up arrow",
+  "Duplicate line below": "alt + shift + down arrow",
+  Refractor: "ctrl + shift + R",
+  Rename: "F2",
+  "Goto definition": "F12",
+  "Multi-cursor": "ctrl + D",
+  "Quick find files": "ctrl + P",
 };
 
 var shortcutsKeys = Object.keys(shortcuts);
 
-
 function App() {
   const [userInputMeaning, shortcutMeaning] = useState("");
 
-  function shortcutInput(e){
+  function shortcutInput(e) {
     var Input = e.target.value;
     var meaning = shortcuts[Input];
-    if(meaning == undefined){
+    if (meaning == undefined) {
       meaning = "Not found in our database";
     }
 
     shortcutMeaning(meaning);
   }
 
-  function shortcutClickHandler(shortcut){
+  function shortcutClickHandler(shortcut) {
     var meaning = shortcuts[shortcut];
     shortcutMeaning(meaning);
   }
@@ -57,27 +56,27 @@ function App() {
   return (
     <div className="App">
       <body>
-      <h2>SHORTCUT GENERATOR</h2>
-      <h3>
-        for Developers
-      </h3>
+        <h2>SHORTCUT GENERATOR</h2>
+        <h3>for Developers</h3>
 
-      <input onChange={shortcutInput} />
-      <h3>{userInputMeaning}</h3>
-      <h3>What we know</h3>
+        <input onChange={shortcutInput} />
+        <h3>{userInputMeaning}</h3>
+        <h3>What we know</h3>
 
-{shortcutsKeys.map(function(keys){
-  return (
-    <button class="button-30" role="button" onClick={
-      () => shortcutClickHandler(keys)}>
-      {keys}
-    </button>
-  )
-})}
+        {shortcutsKeys.map(function (keys) {
+          return (
+            <button
+              class="button-30"
+              role="button"
+              onClick={() => shortcutClickHandler(keys)}
+            >
+              {keys}
+            </button>
+          );
+        })}
       </body>
     </div>
   );
 }
 
 export default App;
-
